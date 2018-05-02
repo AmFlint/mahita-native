53c1456eb099d1ec29543dfec6dcbfe9479e338d
AmFlint <amasselot@hotmail.com>
2018-05-02 23:42:46 +0200

    [Redux/Action/Reducer] Course filtering

--- src/reducers/course_filter.js
+++ src/reducers/course_filter.js
@@ -1,4 +1,10 @@
-import {CHANGE_FILTER_CATEGORY, CHANGE_FILTER_CLASSE, RESET_FILTERS} from '../actions';
+import {
+    ADD_CATEGORIES_FILTER,
+    ADD_CLASSES_FILTER,
+    REMOVE_CATEGORIES_FILTER,
+    REMOVE_CLASSES_FILTER,
+    RESET_FILTERS
+} from '../actions';
 
 const initialFilterState = {
   classe: [],
@@ -8,15 +14,31 @@ const initialFilterState = {
 // Filter Reducer, change the filter for course based on Classe (CP/CE1...) and Category(Maths,Français...)
 export default (state = initialFilterState, action) => {
     switch (action.type) {
-        case CHANGE_FILTER_CLASSE:
+        case ADD_CLASSES_FILTER:
             return {
                 ...state,
-                classe: action.payload
+                classe: [
+                    ...state.classe,
+                    action.payload
+                ]
             };
-        case CHANGE_FILTER_CATEGORY:
+        case REMOVE_CLASSES_FILTER:
             return {
                 ...state,
-                category: action.payload
+                classe: state.classe.filter(c => c !== action.payload)
+            };
+        case REMOVE_CATEGORIES_FILTER:
+            return {
+                ...state,
+                category: state.category.filter(category => category !== action.payload)
+            };
+        case ADD_CATEGORIES_FILTER:
+            return {
+                ...state,
+                category: [
+                    ...state.category,
+                    action.payload
+                ]
             };
         case RESET_FILTERS:
             return initialFilterState;
