16170717b8e80c70a301b345752827af362afda1
AmFlint <amasselot@hotmail.com>
2018-05-02 16:16:32 +0200

    [Reducers/Filters] Add reducers for courses filters categories/Classes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,8 @@
 import { combineReducers } from 'redux';
-import { classFilter, categoryFilter } from './course_filter';
+import filter from './course_filter';
 import coursesReducer from './courses_reducer';
 
 export default combineReducers({
     courses: coursesReducer,
-    filters: {
-        classe: classFilter,
-        category: categoryFilter
-    }
+    filters: filter
 });
