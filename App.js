981f6cacae2f46ed47a93f309ba545c5ab666cb2
AmFlint <amasselot@hotmail.com>
2018-05-02 13:35:38 +0200

    Base architecture

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import Thunk from 'redux-thunk';
+
+const initialState = {};
+
+const store = createStore(
+    () => {},
+    initialState,
+    applyMiddleware(Thunk)
+);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Provider store={store}>
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Changes you make will automatically reload.</Text>
+            <Text>Shake your phone to open the developer menu.</Text>
+          </View>
+        </Provider>
     );
   }
 }
---
+++ db.js
@@ -0,0 +1,26 @@
+export const categories = [
+  'Maths',
+  'Français',
+  'Géographie',
+  'Histoire'
+];
+
+export const classes = [
+  'CP',
+  'CE1',
+  'CE2',
+  'CM1',
+  'CM2'
+];
+
+export const courses = [
+    {
+        name: "les multiplications",
+        categories: 1,
+        classes: [1, 2]
+    }
+];
+
+export default database = {
+    
+};
