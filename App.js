4aed57cf6dc57df7fe91c64ce678831c9fbc7df3
YorelN <nicolas.leroy@hetic.net>
2018-05-02 16:17:07 +0200

    Create styled Card component

--- App.js
+++ App.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import Thunk from 'redux-thunk';
-import rootReducer from './src/reducers';
-import CourseList from './src/components/CourseList';
-import CourseService from './src/services/CourseService';
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import Thunk from "redux-thunk";
+import rootReducer from "./src/reducers";
+import CourseList from "./src/components/CourseList";
+import CourseService from "./src/services/CourseService";
+import Card from "./src/components/Card";
 const initialState = {};
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(Thunk)
-);
+const store = createStore(rootReducer, initialState, applyMiddleware(Thunk));
 
 export default class App extends React.Component {
   componentWillMount() {
-      const svc = new CourseService();
-      svc.preloadCourses();
+    const svc = new CourseService();
+    svc.preloadCourses();
   }
 
   render() {
     return (
-        <Provider store={store}>
-          <View style={styles.container}>
-            <CourseList />
-          </View>
-        </Provider>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Card />
+        </View>
+      </Provider>
     );
   }
 }
@@ -35,8 +31,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#DEDEDE",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
