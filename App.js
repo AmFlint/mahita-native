fa549b664379a53c9e78510adfbbec0627b23cb7
YorelN <nicolas.leroy@hetic.net>
2018-05-02 17:03:46 +0200

    [Rebase] Merge all conflicts

--- App.js
+++ App.js
@@ -1,60 +1,34 @@
-<<<<<<< HEAD
-
-import React from 'react';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import Thunk from 'redux-thunk';
-import rootReducer from './src/reducers';
-import CourseService from './src/services/CourseService';
-import ClassesService from "./src/services/ClassesService";
-import CategoryService from "./src/services/CategoryService";
-
-import AppWithNavigationState from './src/components/AppNavigator';
-
-=======
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, SafeAreaView } from "react-native";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import Thunk from "redux-thunk";
 import rootReducer from "./src/reducers";
-import CourseList from "./src/components/CourseList";
 import CourseService from "./src/services/CourseService";
-import Card from "./src/components/Card";
->>>>>>> Create styled Card component
+import ClassesService from "./src/services/ClassesService";
+import CategoryService from "./src/services/CategoryService";
+
+import AppWithNavigationState from "./src/components/AppNavigator";
+
 const initialState = {};
 
 const store = createStore(rootReducer, initialState, applyMiddleware(Thunk));
 
 export default class App extends React.Component {
   componentWillMount() {
-<<<<<<< HEAD
-      const svc = new CourseService();
-      const classSvc = new ClassesService();
-      const categorySvc = new CategoryService();
-      svc.preloadCourses();
-      classSvc.preloadClasses();
-      categorySvc.preloadCategories();
-=======
     const svc = new CourseService();
+    const classSvc = new ClassesService();
+    const categorySvc = new CategoryService();
     svc.preloadCourses();
->>>>>>> Create styled Card component
+    classSvc.preloadClasses();
+    categorySvc.preloadCategories();
   }
 
   render() {
     return (
-<<<<<<< HEAD
-        <Provider store={store}>
-            <AppWithNavigationState />
-        </Provider>
-=======
       <Provider store={store}>
-        <View style={styles.container}>
-          <Card />
-        </View>
+        <AppWithNavigationState />
       </Provider>
->>>>>>> Create styled Card component
     );
   }
 }
