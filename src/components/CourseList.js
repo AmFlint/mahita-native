52b214ee1a33bad426fad1fdcf766a8eda3e96b6
AmFlint <amasselot@hotmail.com>
2018-05-02 23:45:00 +0200

    [Component/CourseList] Update component style and logic

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { ScrollView, View, FlatList, Dimensions, StyleSheet } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { fetchCourses } from '../actions';
+import { fetchCourses, fetchClasses, fetchCategories } from '../actions';
+import ListHeader from './common/ListHeader';
+import CourseItem from './Card';
 
-
+const maxWidth = Dimensions.get('window').width;
+// TODO: Watch Dimensions change (event listener) to readapt state.dimensions
 class CourseList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {dimensions: undefined};
+    }
+
+    // Retrieve parent dimensions to properly set list items width
+    onLayout = event => {
+        if (this.state.dimensions) return; // layout was already called
+        let {width, height} = event.nativeEvent.layout;
+        this.setState({dimensions: {width, height}});
+    };
+
     componentWillMount() {
-        console.log('hello');
         this.props.fetchCourses();
+        this.props.fetchCategories();
+        this.props.fetchClasses();
     }
 
     getVisibleCourses() {
         const { classe, category } = this.props.filters;
-        console.log(classe, category);
+        // console.log(classe, category);
         // If not filter is set, return all courses
         if (!classe.length && !category.length) {
             return this.props.courses;
@@ -24,30 +40,61 @@ class CourseList extends Component {
             let visible = true;
 
             // One or more category chosen
-            visible = visible && category.includes(course.category);
+            visible = visible && (category.includes(course.categorie) || !category.length);
 
             // One or more classes chosen
-            visible = visible && classe.reduce( (acc, clas) => {
-                return course.classe.includes(clas) || acc;
-            }, false);
+            visible = visible && (!classe.length || classe.reduce((acc, clas) => {
+                return course.classes.includes(clas) || acc;
+            }, false));
 
             return visible;
         });
     }
 
     render() {
+        // TODO: CHANGE WRAPPER STYLE if number of results > 4 (space-between is breaking)
+        // If dimensions were not set, render empty view and activate onLayout to retrieve parent dimensions
+        if (!this.state.dimensions) {
+            return (<View
+                style={{flex: 1, width: maxWidth * .8, alignSelf: 'stretch'}}
+                onLayout={this.onLayout}
+            >
+            </View>);
+        }
+
+        const listData = this.getVisibleCourses();
+        // Get Parent width (in pixels)
+        const { width } = this.state.dimensions;
+        // calculate itemWidth with margins
+        const itemWidth = width / 4.25;
         return (
-            <View>
-                <Text>Yo</Text>
+            <ScrollView style={styles.container}>
                 <FlatList
-                    data={this.getVisibleCourses()}
-                    renderItem={({item}) => <Text>{item.name}</Text>}
+                    columnWrapperStyle={styles.listWrapper}
+                    data={listData}
+                    renderItem={({item}) =>
+                        <CourseItem
+                            itemWidth={itemWidth}
+                            course={item}
+                        />}
                     keyExtractor={(item) => `${item.id}`}
+                    numColumns={4}
+                    ListHeaderComponent={<ListHeader numResults={listData.length} />}
                 />
-            </View>
+            </ScrollView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+       width: maxWidth * .85
+    },
+    listWrapper: {
+       justifyContent: 'space-between',
+        marginBottom: 20
+    }
+});
 
 const mapStateToProps = (state) => {
   return {
@@ -57,7 +104,11 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({ fetchCourses }, dispatch);
+    return bindActionCreators({
+        fetchCourses,
+        fetchCategories,
+        fetchClasses
+    }, dispatch);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CourseList);
