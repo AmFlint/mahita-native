38222e577aa9a268ad0261e4205f56b0350d4c77
YorelN <nicolas.leroy@hetic.net>
2018-05-03 11:50:38 +0200

    Create base layout

---
+++ src/components/AgendaPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View, Dimensions, Text } from "react-native";
+
+class AgendaPage extends React.Component {
+  render() {
+    return <Text>Agenda</Text>;
+  }
+}
+
+export default AgendaPage;
