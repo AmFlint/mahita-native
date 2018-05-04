983df55f5362191acd14b4be930f94d10bdbf92d
YorelN <nicolas.leroy@hetic.net>
2018-05-04 09:53:35 +0200

    Create alert page

--- src/components/NotificationsPage.js
+++ src/components/NotificationsPage.js
@@ -1,80 +1,69 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
-import styled from 'styled-components';
-import Proptypes from 'prop-types';
-
+import React from "react";
+import { ScrollView } from "react-native";
+import styled from "styled-components";
+import Proptypes from "prop-types";
 
 class NotificationsPage extends React.Component {
-
   state = {
     notifications: {}
   };
 
-
-
   render() {
     return (
-        <ScrollView>
-      <PageContainer>
-        <Notifications
-          time="16/12/1994 à 16h20"
-          content="Nouveau conseil d'apprentissage sur le document CM2 --
+      <ScrollView>
+        <PageContainer>
+          <Notifications
+            time="16/12/1994 à 16h20"
+            content="Nouveau conseil d'apprentissage sur le document CM2 --
           Mathématiques -- Fractions"
-          active={true}
-        />
-        <Notifications
-          time="16/12/1994 à 16h20"
-          content="Nouveau conseil d'apprentissage sur le document CM2 --
+            active={true}
+          />
+          <Notifications
+            time="16/12/1994 à 16h20"
+            content="Nouveau cours pour de Français pour les CE2 disponibles"
+          />
+          <Notifications
+            time="16/12/1994 à 16h20"
+            content="Deux exercices ajouter par des contributeurs dans le chapitre CM1 -- Histoire -- Le temps des rois"
+            active={true}
+          />
+          <Notifications
+            time="16/12/1994 à 16h20"
+            content="Nouveau conseil d'apprentissage sur le document CM1 --
+          Histoire -- Découvrir le(s) lieu(x) où j'habite"
+          />
+          <Notifications
+            time="16/12/1994 à 16h20"
+            content="Nouveau conseil d'apprentissage sur le document CM2 --
           Mathématiques -- Fractions"
-        />
-        <Notifications
-          time="16/12/1994 à 16h20"
-          content="Nouveau conseil d'apprentissage sur le document CM2 --
-          Mathématiques -- Fractions"
-          active={true}
-        /><Notifications
-        time="16/12/1994 à 16h20"
-        content="Nouveau conseil d'apprentissage sur le document CM2 --
-          Mathématiques -- Fractions"
-      />
-        <Notifications
-          time="16/12/1994 à 16h20"
-          content="Nouveau conseil d'apprentissage sur le document CM2 --
-          Mathématiques -- Fractions"
-          active={true}
-        />
-        <Notifications
-          time="16/12/1994 à 16h20"
-          content="Nouveau conseil d'apprentissage sur le document CM2 --
-          Mathématiques -- Fractions"
-        />
-        <Notifications
-          time="16/12/1994 à 16h20"
-          content="Nouveau conseil d'apprentissage sur le document CM2 --
-          Mathématiques -- Fractions"
-          active={true}
-        />
-      </PageContainer>
-        </ScrollView>
+            active={true}
+          />
+          <Notifications
+            time="16/12/1994 à 16h20"
+            content="Nouveau cours pour de Français pour les CE2 disponibles"
+          />
+          <Notifications
+            time="16/12/1994 à 16h20"
+            content="Deux exercices ajouter par des contributeurs dans le chapitre CM1 -- Histoire -- Le temps des rois"
+            active={true}
+          />
+          <Notifications
+            time="16/12/1994 à 16h20"
+            content="Nouveau conseil d'apprentissage sur le document CM1 --
+          Histoire -- Découvrir le(s) lieu(x) où j'habite"
+          />
+        </PageContainer>
+      </ScrollView>
     );
   }
 }
 
-
-
-
-
 const PageContainer = styled.View`
   padding: 40px;
-  
+
   display: flex;
   align-items: center;
-  
-  
-
 `;
-
-
 
 const Time = styled.Text`
   font-size: 14px;
@@ -93,38 +82,30 @@ const Content = styled.Text`
 `;
 
 const Container = styled.View`
-  background-color: ${ props => props.active ? '#FEF' : '#FFF' };
+  background-color: ${props =>
+    props.active ? "rgba(0, 0, 0, 0.03)" : "#FFFFFF"};
   flex-wrap: nowrap;
   flex-direction: row;
 `;
 
-
 const Icon = styled.View`
   width: 50px;
   height: 50px;
-  
+
+  align-items: center;
+  justify-content: center;
   margin: 16px;
-  
-  background:#000; 
+
+  background: #000;
 `;
-
-
-
-
 
 function Notifications({ icon, time, content, active }) {
   return (
     <Container active={active}>
-      <Icon>
-        {icon}
-      </Icon>
+      <Icon>{icon}</Icon>
       <ContentContainer>
-        <Time>
-          {time}
-        </Time>
-        <Content>
-          {content}
-        </Content>
+        <Time>{time}</Time>
+        <Content>{content}</Content>
       </ContentContainer>
     </Container>
   );
@@ -136,6 +117,5 @@ Notifications.proptypes = {
   content: Proptypes.string.isRequired,
   active: Proptypes.bool
 };
-
 
 export default NotificationsPage;
