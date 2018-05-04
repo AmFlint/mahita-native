d2d890ebb63546d69625c6b0a6ec8ed6cb0ac4a2
YorelN <nicolas.leroy@hetic.net>
2018-05-04 10:05:11 +0200

    Notifications finished

--- src/components/NotificationsPage.js
+++ src/components/NotificationsPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Image } from "react-native";
 import styled from "styled-components";
 import Proptypes from "prop-types";
 
@@ -15,42 +15,114 @@ class NotificationsPage extends React.Component {
           <Notifications
             time="16/12/1994 à 16h20"
             content="Nouveau conseil d'apprentissage sur le document CM2 --
-          Mathématiques -- Fractions"
+                    Mathématiques -- Fractions"
             active={true}
+            icon={
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{
+                  uri:
+                    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/31963890_2374444179247952_4188247597896433664_n.png?_nc_cat=0&oh=ca83bbc0739c663165e8edb731e62b40&oe=5B90774C"
+                }}
+              />
+            }
           />
           <Notifications
             time="16/12/1994 à 16h20"
             content="Nouveau cours pour de Français pour les CE2 disponibles"
+            icon={
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{
+                  uri:
+                    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/31947394_2374452759247094_8231893035735056384_n.png?_nc_cat=0&oh=583a8e19150c09cb7b8dde983f235760&oe=5B98B6CC"
+                }}
+              />
+            }
           />
           <Notifications
             time="16/12/1994 à 16h20"
             content="Deux exercices ajouter par des contributeurs dans le chapitre CM1 -- Histoire -- Le temps des rois"
             active={true}
+            icon={
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{
+                  uri:
+                    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/31947394_2374452759247094_8231893035735056384_n.png?_nc_cat=0&oh=583a8e19150c09cb7b8dde983f235760&oe=5B98B6CC"
+                }}
+              />
+            }
           />
           <Notifications
             time="16/12/1994 à 16h20"
             content="Nouveau conseil d'apprentissage sur le document CM1 --
-          Histoire -- Découvrir le(s) lieu(x) où j'habite"
+                    Histoire -- Découvrir le(s) lieu(x) où j'habite"
+            icon={
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{
+                  uri:
+                    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/31963890_2374444179247952_4188247597896433664_n.png?_nc_cat=0&oh=ca83bbc0739c663165e8edb731e62b40&oe=5B90774C"
+                }}
+              />
+            }
           />
           <Notifications
             time="16/12/1994 à 16h20"
             content="Nouveau conseil d'apprentissage sur le document CM2 --
-          Mathématiques -- Fractions"
+                    Mathématiques -- Fractions"
             active={true}
+            icon={
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{
+                  uri:
+                    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/31947394_2374452759247094_8231893035735056384_n.png?_nc_cat=0&oh=583a8e19150c09cb7b8dde983f235760&oe=5B98B6CC"
+                }}
+              />
+            }
           />
           <Notifications
             time="16/12/1994 à 16h20"
             content="Nouveau cours pour de Français pour les CE2 disponibles"
+            icon={
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{
+                  uri:
+                    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/31963890_2374444179247952_4188247597896433664_n.png?_nc_cat=0&oh=ca83bbc0739c663165e8edb731e62b40&oe=5B90774C"
+                }}
+              />
+            }
           />
           <Notifications
             time="16/12/1994 à 16h20"
             content="Deux exercices ajouter par des contributeurs dans le chapitre CM1 -- Histoire -- Le temps des rois"
             active={true}
+            icon={
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{
+                  uri:
+                    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/31947394_2374452759247094_8231893035735056384_n.png?_nc_cat=0&oh=583a8e19150c09cb7b8dde983f235760&oe=5B98B6CC"
+                }}
+              />
+            }
           />
           <Notifications
             time="16/12/1994 à 16h20"
             content="Nouveau conseil d'apprentissage sur le document CM1 --
-          Histoire -- Découvrir le(s) lieu(x) où j'habite"
+                    Histoire -- Découvrir le(s) lieu(x) où j'habite"
+            icon={
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{
+                  uri:
+                    "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/31963890_2374444179247952_4188247597896433664_n.png?_nc_cat=0&oh=ca83bbc0739c663165e8edb731e62b40&oe=5B90774C"
+                }}
+              />
+            }
           />
         </PageContainer>
       </ScrollView>
@@ -95,8 +167,6 @@ const Icon = styled.View`
   align-items: center;
   justify-content: center;
   margin: 16px;
-
-  background: #000;
 `;
 
 function Notifications({ icon, time, content, active }) {
