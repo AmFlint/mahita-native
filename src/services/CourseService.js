a50f58875b21a18d7be00baacf61dbf3ec818837
AmFlint <amasselot@hotmail.com>
2018-05-06 13:45:18 +0200

    [Service/Courses] Update courses service to use courses backend

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,40 +1,17 @@
 import { AsyncStorage } from 'react-native';
 import { courses } from '../../db';
+import { getCourses, saveCourse, preloadDefaultCourses } from '../backend';
 
 const storageKey = 'testing_courses';
 
 export default class CourseService {
     getCourses = async () => {
-        let courses = [];
-        try {
-            const storageCourses = await AsyncStorage.getItem(storageKey);
-            courses = JSON.parse(storageCourses);
-        } catch (e) {
-            console.log('could not fetch courses, got error: ' + e);
-            courses = [];
-        }
-        return courses;
+        return await getCourses();
     };
 
     preloadCourses = async () => {
-        let storageCourses = await AsyncStorage.getItem(storageKey);
-        await AsyncStorage.setItem(storageKey, JSON.stringify(courses));
+        await preloadDefaultCourses();
     };
-
-    setCourse = async (courses) => {
-        let storageCourses = '';
-        try {
-            storageCourses = JSON.stringify(courses);
-        } catch (e) {
-            console.log('Error saving courses ', e);
-            storageCourses = '';
-        }
-        if (storageCourses) {
-            await AsyncStorage.setItem(storageKey, storageCourses);
-            return true;
-        }
-        return false;
-    }
 
     saveCourse = async (name, content, classeId, categoryId) => {
         const course = {
@@ -43,21 +20,9 @@ export default class CourseService {
             classes: [classeId],
             categorie: categoryId
         }
-        const courses = await this.getCourses();
 
-        // Get ID
-        let nextId = courses.reduce((aggr , course) => {
-            return course.id > aggr ? course.id : aggr;
-        }, 0);
-        nextId++;
-
-        console.log(nextId);
-        course.id = nextId;
-
-        courses.push(course);
-        const saved = await this.setCourse(courses);
-
-        // Handle not saved
-        return courses;
+        // Call to backend (here, AsyncStorage manipulation)
+        const storageCourse = await saveCourse(course);
+        return storageCourse;
     }
 };
